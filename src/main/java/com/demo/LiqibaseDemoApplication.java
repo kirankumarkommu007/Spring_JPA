6173ed6779ed0d04c5de0fb6d696f13df9d73b07
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2024-05-28 13:07:25 +0530

    Liquibase With yml file

---
+++ src/main/java/com/demo/LiqibaseDemoApplication.java
@@ -0,0 +1,38 @@
+package com.demo;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+
+
+import liquibase.Liquibase;
+import liquibase.integration.spring.SpringLiquibase;
+
+@SpringBootApplication
+public class LiqibaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiqibaseDemoApplication.class, args);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:mysql://localhost:3306/liqibase")
+                .username("root")
+                .password("Master@123")
+                .build();
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.yaml");
+        return liquibase;
+    }
+}
